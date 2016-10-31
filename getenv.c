30c6f634541e2429fc5a9be5237f76bec5bd717a
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-10-31 14:19:58 +0900

    Add files via upload

---
+++ getenv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", getenv("HOME"));
+	printf("%s\n", getenv("PS1"));
+	printf("%s\n", getenv("PATH"));
+	printf("$%s\n", getenv("LD_LIBRARY_PATH"));
+	exit(0);
+}
